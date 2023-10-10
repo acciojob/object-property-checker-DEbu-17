1e4e95e3a3ec181b120faaae439829847e1dc214
DEbu-17 <98536414+DEbu-17@users.noreply.github.com>
2023-10-10 19:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let keyName = Object.keys(sampleObject);
+	for(let i=0; i<keyName.length; i++){
+		if(keyName[i] == key){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+// const key = prompt("Enter Key.");
 alert(hasKey(key));
